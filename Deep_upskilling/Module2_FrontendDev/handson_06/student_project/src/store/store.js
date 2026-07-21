aff1faf2e75a5a75154e9400ac60c6ec142a10a6
Yogesh Venkatapathi <yy264200@gmail.com>
2026-07-21 20:24:15 +0530

    [Deep_upskilling] Add Store exercise implementation

---
+++ Deep_upskilling/Module2_FrontendDev/handson_06/student_project/src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore }
+from "@reduxjs/toolkit";
+
+import enrollmentReducer
+from "./enrollmentSlice";
+
+export const store =
+  configureStore({
+
+    reducer: {
+      enrollment:
+        enrollmentReducer
+    }
+
+  });
