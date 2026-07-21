565e28de3ff96132b822d5101b0ca061321d1c34
Yogesh Venkatapathi <yy264200@gmail.com>
2026-07-21 20:24:20 +0530

    [Deep_upskilling] Add Main exercise implementation

---
+++ Deep_upskilling/Module2_FrontendDev/handson_10/centralAPI/src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.config.errorHandler =
+    (err) => {
+
+        console.error(
+            "Global Error:",
+            err
+        );
+
+    };
+
+app.mount("#app");
