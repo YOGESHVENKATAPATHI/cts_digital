b81cdad6bfcdc811d4f9458ed5530a9771333f48
Yogesh Venkatapathi <yy264200@gmail.com>
2026-07-21 20:24:51 +0530

    [Upskilling_coding_exercises] Add Ex No 3 exercise implementation

---
+++ Upskilling_coding_exercises/SQL/ex_no_3.sql
@@ -0,0 +1,8 @@
+SELECT
+    u.user_id,
+    u.full_name
+FROM Users u
+LEFT JOIN Registrations r
+ON u.user_id = r.user_id
+WHERE r.registration_date IS NULL
+OR r.registration_date < CURDATE() - INTERVAL 90 DAY;
